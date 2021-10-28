4b8e07b0142b761e087c5eeee0f1ba7ef5882dd6
raja24 <rajapalacherla24@gmail.com>
2021-10-28 15:52:16 +0530

    revert

--- log_app/models.py
+++ log_app/models.py
@@ -7,6 +7,6 @@ class Attende(models.Model):
     #room_name = models.CharField(max_length=4, blank=False, default='C424')
     uid = models.IntegerField(validators=[MinValueValidator(1000)], blank=False)
     purpose = models.CharField(max_length=12, blank=False)
-    date = models.DateField(auto_now_add=True, null=True)
+    date = models.DateField(auto_now_add=True)
     in_time = models.TimeField(auto_now_add=True, null=False)
     out_time = models.TimeField(null=True)
