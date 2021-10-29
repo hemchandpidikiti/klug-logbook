9ecc2b23c906ef65cb32a6ca3293fb3acd0bb062
mounikapadala <mounikapadala8@gmail.com>
2021-10-29 12:24:00 +0530

    added RFID MODULE

---
+++ rfid/ser.py
@@ -0,0 +1,38 @@
+import serial
+import time
+import schedule
+
+def main_func():
+    arduino=serial.Serial('COM3',9600)
+    print('Established serial connection to Arduino')
+    arduino_data = arduino.readline()
+
+    decoded_values = str(arduino_data[0:len(arduino_data)].decode("utf-8"))
+    #list_values = decoded_values.split('x')
+
+    #for item in list_values:
+        #list_in_floats.append(float(item))
+
+    #print(f'Collected readings from Arduino: {list_in_floats}')
+    print(f'Collected readings from Arduino: {decoded_values}')
+    '''arduino_data = 0
+    list_in_floats.clear()
+    list_values.clear()
+    arduino.close()
+    print('Connection closed')
+    print('<----------------------------->')
+
+
+# ----------------------------------------Main Code------------------------------------
+# Declare variables to be used
+list_values = []
+list_in_floats = []'''
+
+print('Program started')
+
+# Setting up the Arduino
+schedule.every(3).seconds.do(main_func)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
