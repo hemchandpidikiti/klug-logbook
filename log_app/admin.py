d58f4e78209ec53d82973d6e732dc4e5ed565bc9
raja2404 <rajapalacherla24@gmail.com>
2022-07-22 17:37:28 +0530

    added avanced search query in admin

--- log_app/admin.py
+++ log_app/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Attende, Master
 
 # Register your models here.
-admin.site.register(Attende)
-admin.site.register(Master)
+#admin.site.register(Attende)
+@admin.register(Attende)
+class AttendeAdmin(admin.ModelAdmin):
+    #fields = ['room_name', 'uname', 'uid', 'purpose']
+    list_filter = ['room_name', 'date']
+
+#admin.site.register(Master)
+@admin.register(Master)
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ['uid', 'name', 'rfid_id']
+    search_fields = ['uid', 'name', 'rfid_id']
